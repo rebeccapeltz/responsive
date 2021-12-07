ad1cab18b8970d4e6e2c57b12bd4b67c411ab37b
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-12-07 13:57:45 -0800

    2 cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 /*!
-* Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  var cl = cloudinary.Cloudinary.new({ cloud_name: "pictures77" });
+  // replace 'demo' with your cloud name in the line above
+  cl.responsive();
+});
